305c4d1eca2a573433c7ebfe3ec867bf0b1327e0
Hanif Fakira <fakira.work@gmail.com>
2016-09-05 09:17:53 +0100

    US1 & 2 Added addProduct to DOM

--- public/js/Main.js
+++ public/js/Main.js
@@ -8,12 +8,41 @@ function printStockList(){
   shop.showStock().forEach(function(product, index){
     return document.getElementById("stock-list").innerHTML +=
       "<div class='item'>" +
-        '<p>Name:'            + product.name     +'</p>' +
-        '<p>£'                + product.price    +'</p>' +
-        '<p>Colour '          + product.color    +'</p>' +
-        '<p>Description '     + product.category +'</p>' +
-        '<p>Amount in Stock ' + product.quantity +'</p>' +
-        "<input class='add-btn' type='button' value='ADD PRODUCT'></input>" +
+          _productDetails(product) +
+          _addProductToCartButton(index) +
       '</div>';
   });
+}
+
+function addProduct(index) {
+  _clearProductsAndCart();
+
+  product = shop.showStock()[index];
+  shop.addToCart(product);
+  printStockList();
+
+  document.getElementById("shopping-cart").innerHTML += '<h1>SHOPPING CART</h1>';
+
+  shop.showCart().forEach(function(product, index){
+    return document.getElementById("shopping-cart").innerHTML +=
+      "<div class='item'>" + _productDetails(product) +
+      '</div>';
+  });
+}
+
+function _productDetails(product){
+  return  '<p>Name:'            + product.name     +'</p>' +
+          '<p>£'                + product.price    +'</p>' +
+          '<p>Colour '          + product.color    +'</p>' +
+          '<p>Description '     + product.category +'</p>' +
+          '<p>Amount in Stock ' + product.quantity +'</p>';
+}
+
+function _addProductToCartButton(index) {
+  return '<input class="add-btn" type="button" onclick="addProduct('+index+')" value="ADD PRODUCT"></input>';
+}
+
+function _clearProductsAndCart() {
+  document.getElementById("stock-list").innerHTML = "";
+  document.getElementById("shopping-cart").innerHTML = "";
 }

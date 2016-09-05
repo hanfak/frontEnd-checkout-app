a5571cdeb27e4f76feb526e5d2481ae89a1f8a65
Hanif Fakira <fakira.work@gmail.com>
2016-09-05 11:36:51 +0100

    US3 linked to DOM

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,25 +1,26 @@
 var shop = new Shop(new StockList());
 
 document.addEventListener("DOMContentLoaded", function() {
-  _printStockAndCartItems();
+  _printItemsAndTotal();
 });
 
 function addProduct(index) {
   _clearProductsAndCart();
   product = shop.showStock()[index];
   shop.addToCart(product);
-  _printStockAndCartItems();
+  _printItemsAndTotal();
 }
 
 function removeProduct(index) {
   _clearProductsAndCart();
   product = shop.showCart()[index];
   shop.removeFromCart(product);
-  _printStockAndCartItems();
+  _printItemsAndTotal();
 }
 
 // Private methods
-function _printStockAndCartItems(){
+function _printItemsAndTotal(){
+  _totalOfCart();
   _printStockItems();
   _printShoppingCartItems();
 }
@@ -40,20 +41,28 @@ function _removeProductFromCartButton(index) {
   return '<input class="remove-btn" type="button"  onclick="removeProduct('+index+')" value="REMOVE PRODUCT"></input>';
 }
 
-function _shoppingCartHeading(){
-  if(shop.showCart().length > 0){
-    return document.getElementById("shopping-cart").innerHTML += '<h1>SHOPPING CART</h1>';
-  }
-}
-
 function _printStockItems() {
   shop.showStock().forEach(function(product, index){
     return document.getElementById("stock-list").innerHTML +=
       "<div class='item'>" +
-          _productDetails(product) +
-          _addProductToCartButton(index) +
+        _productDetails(product) +
+        _addProductToCartButton(index) +
       '</div>';
   });
+}
+
+function _totalOfCart() {
+  if(shop.showCart().length > 0){
+    return document.getElementById("total").innerHTML +=
+      '<h1>TOTAL</h1>' +
+      '<p>£' + shop.totalOfCart() + '</p>';
+  }
+}
+
+function _shoppingCartHeading(){
+  if(shop.showCart().length > 0){
+    return document.getElementById("shopping-cart").innerHTML += '<h1>SHOPPING CART</h1>';
+  }
 }
 
 function _printShoppingCartItems() {
@@ -63,11 +72,12 @@ function _printShoppingCartItems() {
     "<div class='item'>"                  +
       _productDetails(product)            +
       _removeProductFromCartButton(index) +
-     '</div>';
+    '</div>';
   });
 }
 
 function _clearProductsAndCart() {
   document.getElementById("stock-list").innerHTML = "";
   document.getElementById("shopping-cart").innerHTML = "";
+  document.getElementById("total").innerHTML = "";
 }
